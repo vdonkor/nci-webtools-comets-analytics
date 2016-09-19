ee2670b1a69f5ffc130bdbf5cd1c9419073d7136
Wesley Obenshain <wesley.obenshain@nih.gov>
2016-09-19 15:10:53 -0400

    reorganize folder structure

--- comets/javascript/appViews.js
+++ comets/javascript/appViews.js
@@ -538,7 +538,7 @@ $(function () {
     $('#logoutBtn').on('click', function (e) {
         e.preventDefault();
         var path = window.location.href;
-        window.location = "/auth0_redirect?logout=" + encodeURIComponent(path.substring(0,path.lastIndexOf('/')));
+        window.location = "/auth0_redirect?logout=" + encodeURIComponent(path.substring(0,path.lastIndexOf('/'))+"/public/logout.html");
     });
     templates = $.ajax({
         type: "GET",
