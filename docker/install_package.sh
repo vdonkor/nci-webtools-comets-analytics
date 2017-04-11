199b76d4cb67655261a0e397212e7c23f9db0838
park-brian <re.public@outlook.com>
2017-04-11 12:46:41 -0400

    update comets compose files

--- docker/install_package.sh
+++ docker/install_package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker exec --user root comets bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
-docker exec --user root processor bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
+docker exec --user root comets_devel bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
+docker exec --user root comets_processor bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
 sleep 5
 docker restart comets
 docker restart processor
