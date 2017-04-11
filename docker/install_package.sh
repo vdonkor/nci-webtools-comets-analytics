c9673be47043076f9f59a3ec6229aafab64ee64c
park-brian <re.public@outlook.com>
2017-04-11 12:21:56 -0400

    update install_package.sh

--- docker/install_package.sh
+++ docker/install_package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker exec --user root comets bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
+docker exec --user root processor bash -c 'R CMD INSTALL /deploy/app/restricted/rcode'
 sleep 5
 docker restart comets
+docker restart processor
