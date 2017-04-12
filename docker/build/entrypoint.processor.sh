547dd114ea659b44ff508624c42618fea3d68a02
park-brian <re.public@outlook.com>
2017-04-12 11:10:16 -0400

    change ownership in entrypoint

--- docker/build/entrypoint.processor.sh
+++ docker/build/entrypoint.processor.sh
@@ -3,6 +3,9 @@
 # install comets package if directory does not exist
 [[ ! -d /usr/lib64/R/library/COMETS ]] && install_comets_package.sh
 
+# change ownership of deployment directory
+chown -R ncianalysis:ncianalysis /deploy
+
 # start python development server
 pushd /deploy/app
 python RequestProcessor.py
--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -3,6 +3,9 @@
 # install comets package if directory does not exist
 [[ ! -d /usr/lib64/R/library/COMETS ]] && install_comets_package.sh
 
+# change ownership of deployment directory
+chown -R ncianalysis:ncianalysis /deploy
+
 # start wsgi server
 pushd /deploy
 mod_wsgi-express start-server /deploy/app/deploy.wsgi \
