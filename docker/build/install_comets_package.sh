1ab6079fc46259d7a8d98bb66bf864136e1765d1
park-brian <re.public@outlook.com>
2017-04-12 10:38:11 -0400

    add sleep after starting installation script

--- docker/build/install_comets_package.sh
+++ docker/build/install_comets_package.sh
@@ -5,3 +5,5 @@ COMETS_INSTALL_LOG=/deploy/logs/update_comets_package.log
 
 R -q -e "devtools::document('$COMETS_DIR', roclets=c('rd', 'collate', 'namespace'))" >> $COMETS_INSTALL_LOG
 R CMD INSTALL $COMETS_DIR --no-html >> $COMETS_INSTALL_LOG  2>&1
+
+sleep 10
