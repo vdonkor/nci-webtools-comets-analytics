7ebd27788dd21b2098365e6ae34d14b0e38ab462
park-brian <re.public@outlook.com>
2017-04-14 11:29:05 -0400

    update install_package script

--- docker/build/install_comets_package.sh
+++ docker/build/install_comets_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove lock
-rm -f /usr/lib64/R/library/00LOCK-rcode
+rm -rf /usr/lib64/R/library/00LOCK-rcode
 
 COMETS_DIR=/deploy/app/restricted/rcode
 COMETS_INSTALL_LOG=/deploy/logs/update_comets_package.log
