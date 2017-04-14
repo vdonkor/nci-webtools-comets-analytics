ef21120b16d41973c22486ce81ec6005230eed08
park-brian <re.public@outlook.com>
2017-04-14 12:32:04 -0400

    update installation script

--- docker/build/install_comets_package.sh
+++ docker/build/install_comets_package.sh
@@ -6,6 +6,8 @@ rm -rf /usr/lib64/R/library/00LOCK-rcode
 COMETS_DIR=/deploy/app/restricted/rcode
 COMETS_INSTALL_LOG=/deploy/logs/update_comets_package.log
 
+echo "Ensuring COMETS package is installed..."
+
 R -q -e "devtools::document('$COMETS_DIR', roclets=c('rd', 'collate', 'namespace'))" > $COMETS_INSTALL_LOG  2>&1
 R CMD INSTALL $COMETS_DIR --no-html >> $COMETS_INSTALL_LOG  2>&1
 
